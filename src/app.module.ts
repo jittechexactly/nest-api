faed19607c103cd13c4e001fbbd60b82cd47c4a2
Jit <jit.techexactly@gmail.com>
2026-03-05 17:31:45 +0530

    feat(auth): add login endpoint and DTO for authentication
    - Implemented a login endpoint in AuthController.
    - Created LoginDto for validating login requests with email and password.
    - Added JWT payload interface for better type safety.
    - Integrated TypeORM with MySQL in AppModule.
    - Set up global validation pipes in main.ts for request validation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,25 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Matrix@2025',
+      database: 'nest-api',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
