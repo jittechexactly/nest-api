c726906005ee58541bf50a2450a02e9e5888aa64
Jit <jit.techexactly@gmail.com>
2026-03-05 17:57:49 +0530

    feat(auth): implement login method with DTO and response structure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import type { AuthResponse } from './interfaces/auth-response.interface';
 
 @Controller('auth')
 export class AuthController {
-    
+
     @Get('login')
-    login() {
-        return 'Login';
+    login(@Body() loginDto: LoginDto): AuthResponse {
+        return {
+            accessToken: "hjdhdd",
+            refreshToken: "sdjjsnds",
+            tokenType: "Bearer",
+            expiresIn: 900,
+        };
     }
 }
