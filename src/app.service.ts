fb1dedf1a53c57877f8d04c837467670e6984b32
Jit <jit.techexactly@gmail.com>
2026-03-06 12:30:11 +0530

    feat(app): update getHello method to return an object and add index view template

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Jit';
+
+  getHello(): { name: string } {
+    return { name: 'NestJS' };
   }
 }
