c21e3702983e2b78eb6421a20c25c2d4cc9e1da0
Jit <jit.techexactly@gmail.com>
2026-03-17 15:24:32 +0530

    middleware response changes

--- src/middleware/authentication/authentication.middleware.ts
+++ src/middleware/authentication/authentication.middleware.ts
@@ -1,16 +1,20 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
+import { ResponseService } from 'src/modules/response/service/response.service';
 
 @Injectable()
 export class AuthenticationMiddleware implements NestMiddleware {
-  constructor(private readonly jwtService: JwtService) { }
+  constructor(private readonly jwtService: JwtService, private readonly responService: ResponseService) { }
 
   use(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers['authorization'];
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      throw new UnauthorizedException('No token provided!');
+      throw new HttpException(
+        this.responService.response(false, "Something went wrong!", {}),
+        HttpStatus.FORBIDDEN
+      );
     }
 
     const token = authHeader.split(' ')[1];
@@ -22,7 +26,10 @@ export class AuthenticationMiddleware implements NestMiddleware {
       req['user'] = decoded;
       next();
     } catch (error) {
-      throw new UnauthorizedException('Invalid or expired token!');
+      throw new HttpException(
+        this.responService.response(false, "Login expired!", {}),
+        HttpStatus.UNAUTHORIZED
+      );
     }
   }
 }
